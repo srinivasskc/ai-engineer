47701c767397056e1e2f488e5776599e8ad60654
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-06-22 09:30:19 +0530

    for loops

---
+++ python_tau/for_loop.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Mango", "Banana"]
+
+for fruit in fruits:
+    print("Would you like " + fruit + " ?")
+    print(f"Yes, I like {fruit}")
+
+
+for number in range(1, 11):
+    print(number)
