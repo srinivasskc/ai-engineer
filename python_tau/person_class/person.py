f5e8c4e7bf6bd3340eef22eb148000b0947fa928
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-06-24 10:48:50 +0530

    Inheritance - Person, Enemy Class

--- python_tau/person_class/person.py
+++ python_tau/person_class/person.py
@@ -39,8 +39,8 @@ class Person:
             print(f"{self.firstName} is unconsicousness")
 
 
-Srinivas = Person("Srinivas", "Kadiyala", 75, status=True)
-Vasu = Person("Vasu", "Kadiyala", 100, status=False)
+Srinivas = Person("Srinivas", "Kadiyala", 95, status=True)
+Vasu = Person("Vasu", "Kadiyala", 70, status=False)
 
 print(f"{Srinivas.firstName} is my friend? {Srinivas.status}")
 print(f"{Vasu.firstName} is my friend? {Vasu.status}")
@@ -55,3 +55,38 @@ Vasu.status_check()
 
 Srinivas.emotion()
 Vasu.emotion()
+
+
+# Inheritance - Inheriting Person Class to Enemy Class.
+class Enemy(Person):
+    def __init__(self, weapon, firstName, lastName, health, status):
+        super().__init__(firstName, lastName, health, status)
+        self.weapon = weapon
+
+    # Here self is Self Person, other is Other hurting-> person/insult person.
+    def hurt(self, other):
+        if self.weapon == "rock":
+            other.health -= 10
+        elif self.weapon == "stick":
+            other.health -= 5
+        print(f"Now, {other.firstName} health score is:  {other.health}")
+
+    def insult(self, other):
+        if other.health <= 80:
+            print(f"{other.firstName} you are tired and weak")
+
+
+print("====Now, with Alex=====")
+
+Alex = Enemy("rock", "Alex", "Max", 75, status=False)
+Alex.hurt(Srinivas)
+Alex.insult(Srinivas)
+Alex.insult(Vasu)
+
+print("====Now, with Matt=====")
+
+
+Matt = Enemy("stick", "Matt", "Hutt", 85, status=True)
+Matt.hurt(Srinivas)
+Matt.insult(Srinivas)
+Matt.insult(Vasu)
