961f0318827825de203a08343ceb3dd7e3762519
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-06-25 09:55:43 +0530

    pytest with zerodivisionerror

--- pytest_tau/tests/test_math.py
+++ pytest_tau/tests/test_math.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_one_plus_one():
     assert 1 + 1 == 2
 
@@ -7,3 +10,12 @@ def test_one_plus_two():
     b = 1
     c = 0
     assert a + b == c
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    # print(f"\nCaptured message: {e.value}")
+    # command to run: python -m pytest -s
+    assert "division by zero" in str(e.value)
