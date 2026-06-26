b028d0072415bc95be65600287de50b2e39d7878
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-06-26 11:01:40 +0530

    unit test acume

--- pytest_tau/tests/test_acume.py
+++ pytest_tau/tests/test_acume.py
@@ -1,32 +1,37 @@
 import pytest
 from my_package.acume import Accumulator
 
+
 def test_accumulator_init():
     accum = Accumulator()
     assert accum.count == 0
 
+
 def test_accumulator_add_one():
-    accum = Accumulator()       #Arrange - Setting up state.
-    accum.add()                 #Act - Execute the target behavior
-    assert accum.count == 1     #Assert - Verify the outcome. 
+    accum = Accumulator()  # Arrange - Setting up state.
+    accum.add()  # Act - Execute the target behavior
+    assert accum.count == 1  # Assert - Verify the outcome.
+
 
 def test_accumulator_add_three():
     accum = Accumulator()
     accum.add(3)
     assert accum.count == 3
 
+
 def test_accumulator_add_twice():
-    accum  = Accumulator()
+    accum = Accumulator()
     accum.add()
     accum.add()
     assert accum.count == 2
 
+
 def test_accumulator_cannot_set_value_to_count():
     accum = Accumulator()
-    with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter"):
-        accum.count =10
-
-
+    with pytest.raises(
+        AttributeError, match=r"property 'count' of 'Accumulator' object has no setter"
+    ):
+        accum.count = 10
 
 
 # Running the tests:
