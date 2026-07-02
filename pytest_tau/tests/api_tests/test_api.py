5b45b4211df148dcdf151c07e6819144ee084476
Srinivas Kadiyala <srinivas.skc30@gmail.com>
2026-07-02 10:53:23 +0530

    pytest_api_tests_first_test

---
+++ pytest_tau/tests/api_tests/test_api.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+
+@pytest.mark.duckduckgo
+@pytest.mark.api
+def test_duckduckgo_instant_answer_api():
+    # Arrange: Setting up URL.
+    URL = "https://jsonplaceholder.typicode.com/todos/1"
+
+    # Act
+    response = requests.get(url=URL)
+    body = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert "delectus" in body["title"]
